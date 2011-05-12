9cd5bb86309330fa59d5f2f4acf3c2ddc6c5a0e3
Markus Holtermann <markus@markusholtermann.de>
2011-05-12 15:57:44 +0200

    initial project creation

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * main.cpp
+ *
+ *      Author: Markus Holtermann
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+	std::cout << "Totally Unrelated Studios proudly presents\n\tDOMINATOR\n\n";
+	return 0;
+}
