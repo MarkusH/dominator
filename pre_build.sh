f9ada45fdd16c6fecc6eab5ce735a0e68ec64dc0
Markus Holtermann <markus@markusholtermann.de>
2011-05-13 17:55:43 +0200

    add automated moc generating to pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+for f in `find ../src/ -name "moc_*" -and -name "*.cpp" -and -type f`; do
+    rm $f
+done
 
-moc ../include/mainwindow.hpp > ../src/moc_mainwindow.cpp
+for f in `find ../include/ -name "*.hpp" -and -type f -exec grep -l Q_OBJECT {} \;`; do
+    BASENAME=`basename $f`
+    SUBDIR=`dirname ${f:11}`
+    mkdir -p ../src/$SUBDIR
+    moc $f > ../src/$SUBDIR/moc_${BASENAME:0:(-4)}.cpp
+done
