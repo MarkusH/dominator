480520eff6dcd58761a580e6079f6fda3bf5d8e2
Markus Döllinger <markus_doellinger@t-online.de>
2011-05-23 20:20:32 +0200

    Added clock class for Unix. Implementation for Windows required.

---
+++ src/util/Clock.cpp
@@ -0,0 +1,44 @@
+/*
+ * Clock.cpp
+ *
+ *  Created on: May 23, 2011
+ *      Author: markus
+ */
+
+#include <util/Clock.hpp>
+#include <sys/time.h>
+
+namespace util
+{
+
+Clock::Clock()
+{
+	reset();
+}
+
+Clock::~Clock()
+{
+}
+
+double Clock::sysTime() const
+{
+#ifdef _WIN32
+	// TODO: use query performance counter
+#else
+	timeval time = { 0, 0 };
+	gettimeofday(&time, 0);
+	return time.tv_sec + time.tv_usec / 1000000.0;
+#endif
+}
+
+void Clock::reset()
+{
+	m_reference = sysTime();
+}
+
+float Clock::get() const
+{
+	return sysTime() - m_reference;
+}
+
+}
