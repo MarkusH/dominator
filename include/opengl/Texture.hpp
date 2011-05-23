75cc2888fe6b1727390edfcf1bc7d390fa71acb7
Markus Döllinger <markus_doellinger@t-online.de>
2011-05-23 16:25:12 +0200

    Added texture wrapper and texture manager.

---
+++ include/opengl/Texture.hpp
@@ -0,0 +1,124 @@
+/*
+ * Texture.hpp
+ *
+ *  Created on: May 21, 2011
+ *      Author: markus
+ */
+
+#ifndef TEXTURE_HPP_
+#define TEXTURE_HPP_
+
+#include <GL/glew.h>
+#include <tr1/memory>
+#include <string>
+#include <map>
+
+namespace ogl {
+
+class Texture;
+typedef std::tr1::shared_ptr<Texture> TexturePtr;
+
+class Texture {
+public:
+	Texture(GLuint textureID, GLuint stage);
+	virtual ~Texture();
+
+	void setTexParameterf(GLenum pname, GLfloat param);
+	void setTexParameteri(GLenum pname, GLint param);
+	void setTexParameterfv(GLenum pname, const GLfloat* param);
+	void setTexParameteriv(GLenum pname, const GLint* param);
+
+	void setFilter(GLint min, GLint mag);
+	void setWrap(GLint s, GLint t);
+
+	void bind();
+	void unbind();
+
+	static void stage(GLuint stage);
+
+	static TexturePtr load(std::string file, GLuint target = GL_TEXTURE_2D);
+	static TexturePtr create(GLuint target = GL_TEXTURE_2D);
+
+	GLuint m_textureID;
+	GLuint m_target;
+};
+
+
+class TextureMgr : public std::map<std::string, TexturePtr> {
+private:
+	static TextureMgr* s_instance;
+	TextureMgr();
+	TextureMgr(const TextureMgr& other);
+	virtual ~TextureMgr();
+protected:
+public:
+	static TextureMgr& instance();
+	static void destroy();
+
+	TexturePtr add(std::string name, TexturePtr texture);
+	unsigned load(std::string folder);
+	TexturePtr get(std::string name);
+};
+
+
+inline
+void Texture::setTexParameterf(GLenum pname, GLfloat param)
+{
+	glTexParameterf(m_target, pname, param);
+}
+
+inline
+void Texture::setTexParameteri(GLenum pname, GLint param)
+{
+	glTexParameteri(m_target, pname, param);
+}
+
+inline
+void Texture::setTexParameterfv(GLenum pname, const GLfloat* param)
+{
+	glTexParameterfv(m_target, pname, param);
+}
+
+inline
+void Texture::setTexParameteriv(GLenum pname, const GLint* param)
+{
+	glTexParameteriv(m_target, pname, param);
+}
+
+inline
+void Texture::setFilter(GLint min, GLint mag)
+{
+	setTexParameteri(GL_TEXTURE_MIN_FILTER, min);
+	setTexParameterf(GL_TEXTURE_MAG_FILTER, mag);
+}
+
+inline
+void Texture::setWrap(GLint s, GLint t)
+{
+	setTexParameteri(GL_TEXTURE_WRAP_S, s);
+	setTexParameteri(GL_TEXTURE_WRAP_T, t);
+}
+
+inline
+void Texture::bind()
+{
+	glBindTexture(m_target, m_textureID);
+}
+
+inline
+void Texture::unbind()
+{
+	glBindTexture(m_target, 0);
+}
+;
+
+inline
+void Texture::stage(GLuint stage)
+{
+	glActiveTexture(stage);
+}
+
+
+}
+
+#endif /* TEXTURE_HPP_ */
---
+++ src/opengl/Texture.cpp
@@ -0,0 +1,134 @@
+/*
+ * Texture.cpp
+ *
+ *  Created on: May 21, 2011
+ *      Author: markus
+ */
+
+#include <opengl/Texture.hpp>
+#include "stb_image.hpp"
+#include <dirent.h>
+#include <stdexcept>
+
+namespace ogl {
+
+TextureMgr* TextureMgr::s_instance = NULL;
+
+Texture::Texture(GLuint textureID, GLuint target)
+	: m_textureID(textureID), m_target(target)
+{
+}
+
+Texture::~Texture()
+{
+#ifdef _DEBUG
+	std::cout << "delete texture" << std::endl;
+#endif
+	if (glIsTexture(m_textureID))
+		glDeleteTextures(0, &m_textureID);
+}
+
+TexturePtr Texture::load(std::string file, GLuint target)
+{
+	int w, h, c;
+	unsigned char* data = stbi_load(file.c_str(), &w, &h, &c, STBI_rgb_alpha);
+
+	if (!data)
+		return TexturePtr();
+
+    GLuint textureID = 0;
+    glGenTextures(1, &textureID);
+    glBindTexture(target, textureID);
+
+    //glTexImage2D(stage, 0, GL_RGBA, Image.GetWidth(), Image.GetHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, Image.GetPixelsPtr());
+    //gluBuild2DMipmaps(target, GL_RGBA, Image.GetWidth(), Image.GetHeight(), GL_RGBA, GL_UNSIGNED_BYTE, Image.GetPixelsPtr());
+    gluBuild2DMipmaps(target, GL_RGBA, w, h, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+    TexturePtr result(new Texture(textureID, target));
+    return result;
+}
+
+TexturePtr Texture::create(GLuint target)
+{
+    GLuint textureID = 0;
+    glGenTextures(1, &textureID);
+    glBindTexture(target, textureID);
+
+    TexturePtr result(new Texture(textureID, target));
+    return result;
+}
+
+TextureMgr::TextureMgr()
+	: std::map<std::string, TexturePtr>()
+{
+}
+
+TextureMgr::TextureMgr(const TextureMgr& other)
+{
+}
+
+TextureMgr::~TextureMgr()
+{
+}
+
+TextureMgr& TextureMgr::instance()
+{
+	if (!s_instance)
+		s_instance = new TextureMgr();
+	return *s_instance;
+}
+
+void TextureMgr::destroy()
+{
+	if (s_instance)
+		delete s_instance;
+}
+
+TexturePtr TextureMgr::add(std::string name, TexturePtr texture)
+{
+	(*this)[name] = texture;
+	return texture;
+}
+
+unsigned TextureMgr::load(std::string folder)
+{
+	int count = 0;
+	if (folder.at(folder.size() - 1) != '/')
+		folder.append("/");
+
+	DIR* dir = opendir(folder.c_str());
+	dirent* ent;
+
+	if (dir != NULL) {
+		while ((ent = readdir(dir)) != NULL) {
+			std::string file(ent->d_name);
+			if (file.size() > 3) {
+				std::string name = file.substr(0, file.rfind(".", file.size()));
+#ifdef _DEBUG
+				std::cout << "load texture " << name << std::endl;
+#endif
+				TexturePtr texture = Texture::load(folder + file, GL_TEXTURE_2D);
+				add(name, texture);
+			}
+		}
+		closedir(dir);
+	}
+	return count;
+}
+
+TexturePtr TextureMgr::get(std::string name)
+{
+	try {
+		TexturePtr result = this->at(name);
+		return result;
+	} catch (std::out_of_range& err) {
+		TexturePtr result;
+		return result;
+	}
+
+}
+
+}
