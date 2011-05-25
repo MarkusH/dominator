a63192b9628ba95bc872dc10216f48f36b367d15
Markus Holtermann <markus@markusholtermann.de>
2011-05-25 19:23:17 +0200

    implement extended GUI. refers #3

--- include/mainwindow.hpp
+++ include/mainwindow.hpp
@@ -8,33 +8,64 @@
 #define MAINWINDOW_HPP_
 
 #include <QtGui/QApplication>
+#include <QtGui/QMainWindow>
 #include <QtGui/QWidget>
 #include <QtGui/QPushButton>
-#include <QtGui/QSlider>
+#include <QtGui/QSplitter>
+#include <QtGui/QSpinBox>
+#include <QtGui/QComboBox>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QAction>
 #include <QtCore/QTimer>
 
 class Render;
 
-class MainWindow: public QWidget {
+class MainWindow: public QMainWindow {
 Q_OBJECT
 public:
-	MainWindow(QApplication *app, QWidget *parent = 0);
+	MainWindow(QApplication *app);
 
 private slots:
 	void OnClosePressed();
-	void OnRenderWindowPressed();
 
 private:
-	QPushButton *bt_close;
-	QPushButton *bt_render_window;
+	void createMenu();
+	void createToolBox();
+	void createModifyBox();
 
-	QPushButton *bt_rotate_x_plus, *bt_rotate_x_minus;
-	QPushButton *bt_rotate_y_plus, *bt_rotate_y_minus;
-	QPushButton *bt_rotate_z_plus, *bt_rotate_z_minus;
+	QTimer *m_renderTimer;
+	Render *m_renderWindow;
 
-	QTimer *timer_render;
+	QMenu *m_menuFile;
+	QAction *m_new;
+	QAction *m_save;
+	QAction *m_saveas;
+	QAction *m_open;
+	QAction *m_exit;
 
-	Render *render_window;
+	QMenu *m_menuHelp;
+	QAction *m_help;
+	QAction *m_info;
+
+	QWidget *m_toolBox;
+	QComboBox *m_tb3DOjects;
+	QComboBox *m_tbStones;
+	QComboBox *m_tbTemplate;
+	QComboBox *m_tbTexture;
+
+	QWidget *m_modifyBox;
+	QSpinBox *m_mbSizeX;
+	QSpinBox *m_mbSizeY;
+	QSpinBox *m_mbSizeZ;
+	QSpinBox *m_mbLocX;
+	QSpinBox *m_mbLocY;
+	QSpinBox *m_mbLocZ;
+	QSpinBox *m_mbRotX;
+	QSpinBox *m_mbRotY;
+	QSpinBox *m_mbRotZ;
+
+	QSplitter *m_splitter;
 };
 
 #endif /* MAINWINDOW_HPP_ */
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -11,76 +11,60 @@
 #include <QtGui/QPixmap>
 #include <QtGui/QSplashScreen>
 #include <QtGui/QMessageBox>
-#include <QtCore/QString>
-#include <QtGui/QSlider>
-#include <QtCore/QTimer>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QGridLayout>
+#include <QtGui/QLabel>
 
-MainWindow::MainWindow(QApplication *app, QWidget *parent) :
-	QWidget(parent) {
+#include <QtCore/QString>
+#include <QtCore/QList>
+
+MainWindow::MainWindow(QApplication *app) {
 
 	QPixmap pixmap("data/splash.png");
 	QSplashScreen splash(pixmap);
 	splash.show();
 	app->processEvents();
 
-	bt_close = new QPushButton("Close", this);
-	bt_close->setGeometry(10, 10, 120, 30);
-	connect(bt_close, SIGNAL(clicked()), this, SLOT(OnClosePressed()));
-
-	bt_render_window = new QPushButton("Render Window", this);
-	bt_render_window->setGeometry(140, 10, 120, 30);
-	connect(bt_render_window, SIGNAL(clicked()), this,
-			SLOT(OnRenderWindowPressed()));
-
-	bt_rotate_x_plus = new QPushButton("+", this);
-	bt_rotate_x_plus->setGeometry(10, 50, 30, 30);
-
-	bt_rotate_x_minus = new QPushButton("-", this);
-	bt_rotate_x_minus->setGeometry(10, 90, 30, 30);
-
-	bt_rotate_y_plus = new QPushButton("+", this);
-	bt_rotate_y_plus->setGeometry(60, 50, 30, 30);
-
-	bt_rotate_y_minus = new QPushButton("-", this);
-	bt_rotate_y_minus->setGeometry(60, 90, 30, 30);
-
-	bt_rotate_z_plus = new QPushButton("+", this);
-	bt_rotate_z_plus->setGeometry(110, 50, 30, 30);
-
-	bt_rotate_z_minus = new QPushButton("-", this);
-	bt_rotate_z_minus->setGeometry(110, 90, 30, 30);
-
-	render_window = new Render(new QString("data/models/monkey.3ds"), this);
-	render_window->setGeometry(10, 170, 250, 250);
-	render_window->show();
-	connect(bt_rotate_x_plus, SIGNAL(clicked()), render_window,
-			SLOT(setRotationXInc()));
-	connect(bt_rotate_x_minus, SIGNAL(clicked()), render_window,
-			SLOT(setRotationXDec()));
-	connect(bt_rotate_y_plus, SIGNAL(clicked()), render_window,
-			SLOT(setRotationYInc()));
-	connect(bt_rotate_y_minus, SIGNAL(clicked()), render_window,
-			SLOT(setRotationYDec()));
-	connect(bt_rotate_z_plus, SIGNAL(clicked()), render_window,
-			SLOT(setRotationZInc()));
-	connect(bt_rotate_z_minus, SIGNAL(clicked()), render_window,
-			SLOT(setRotationZDec()));
-
-	timer_render = new QTimer(this);
-	timer_render->start();
-	connect(timer_render, SIGNAL(timeout()), render_window, SLOT(updateGL()));
-
-	this->resize(270, 430);
 	this->setWindowTitle("TUStudios - DOMINATOR");
-	this->show();
 
+	createMenu();
+	app->processEvents();
+
+	createModifyBox();
+	app->processEvents();
+
+	createToolBox();
+	app->processEvents();
+
+	m_renderWindow = new Render(new QString("data/models/monkey.3ds"), this);
+	m_renderWindow->setMinimumWidth(400);
+	m_renderTimer= new QTimer(this);
+	m_renderTimer->start();
+	connect(m_renderTimer, SIGNAL(timeout()), m_renderWindow, SLOT(updateGL()));
+	m_renderWindow->show();
+
+	m_splitter = new QSplitter(Qt::Horizontal);
+	m_splitter->insertWidget(0, m_toolBox);
+	m_splitter->insertWidget(1, m_renderWindow);
+
+	QList<int> sizes;
+	sizes.append(200);
+	sizes.append(1300);
+	m_splitter->setSizes(sizes);
+	m_splitter->setStretchFactor(0, 1);
+	m_splitter->setStretchFactor(1, 1);
+	m_splitter->setChildrenCollapsible(false);
+
+	setCentralWidget(m_splitter);
+
+	showMaximized();
 	splash.finish(this);
+
 }
 
 void MainWindow::OnClosePressed() {
 	QMessageBox msgBox;
-	msgBox.setText("Closing the main window");
-	msgBox.setInformativeText("Do you want to exit?");
+	msgBox.setText("Do you want to exit DOMINATOR");
 	msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
 	msgBox.setDefaultButton(QMessageBox::No);
 	int ret = msgBox.exec();
@@ -89,25 +73,109 @@ void MainWindow::OnClosePressed() {
 	}
 }
 
-void MainWindow::OnRenderWindowPressed() {
-	if (render_window->isVisible()) {
-		bt_rotate_x_plus->setEnabled(false);
-		bt_rotate_x_minus->setEnabled(false);
-		bt_rotate_y_plus->setEnabled(false);
-		bt_rotate_y_minus->setEnabled(false);
-		bt_rotate_z_plus->setEnabled(false);
-		bt_rotate_z_minus->setEnabled(false);
-		render_window->hide();
-		timer_render->stop();
-		this->update();
-	} else {
-		timer_render->start();
-		render_window->show();
-		bt_rotate_x_plus->setEnabled(true);
-		bt_rotate_x_minus->setEnabled(true);
-		bt_rotate_y_plus->setEnabled(true);
-		bt_rotate_y_minus->setEnabled(true);
-		bt_rotate_z_plus->setEnabled(true);
-		bt_rotate_z_minus->setEnabled(true);
-	}
+void MainWindow::createMenu() {
+	m_menuFile = menuBar()->addMenu("&File");
+
+	m_new = new QAction("&New", this);
+	m_new->setShortcuts(QKeySequence::New);
+	m_menuFile->addAction(m_new);
+
+	m_open = new QAction("&Open", this);
+	m_open->setShortcuts(QKeySequence::Open);
+	m_menuFile->addAction(m_open);
+
+	m_save = new QAction("&Save", this);
+	m_save->setShortcuts(QKeySequence::Save);
+	m_menuFile->addAction(m_save);
+
+	m_saveas = new QAction("Save &As", this);
+	m_saveas->setShortcuts(QKeySequence::SaveAs);
+	m_menuFile->addAction(m_saveas);
+
+	m_menuFile->addSeparator();
+
+	m_exit = new QAction("E&xit", this);
+	m_exit->setShortcuts(QKeySequence::Quit);
+	connect(m_exit, SIGNAL(triggered()), this, SLOT(OnClosePressed()));
+	m_menuFile->addAction(m_exit);
+
+	m_menuHelp = menuBar()->addMenu("&Help");
+
+	m_help = new QAction("&Help", this);
+	m_help->setShortcuts(QKeySequence::HelpContents);
+	m_menuHelp->addAction(m_help);
+
+	m_info = new QAction("&Info", this);
+	m_menuHelp->addAction(m_info);
+}
+
+void MainWindow::createToolBox() {
+	m_toolBox = new QWidget();
+	QVBoxLayout *layout = new QVBoxLayout();
+
+	layout->addWidget(new QLabel("3D objects:"));
+
+	m_tb3DOjects = new QComboBox();
+	layout->addWidget(m_tb3DOjects);
+
+	layout->addWidget(new QLabel("Stones:"));
+
+	m_tbStones = new QComboBox();
+	layout->addWidget(m_tbStones);
+
+	layout->addWidget(new QLabel("Templates:"));
+
+	m_tbTemplate = new QComboBox();
+	layout->addWidget(m_tbTemplate);
+
+	layout->addWidget(new QLabel("Textures:"));
+
+	m_tbTexture = new QComboBox();
+	layout->addWidget(m_tbTexture);
+
+//	layout->addWidget(m_modifyBox);
+
+	m_toolBox->setLayout(layout);
+	m_toolBox->setMaximumWidth(250);
+}
+
+void MainWindow::createModifyBox() {
+	m_modifyBox = new QWidget();
+	QGridLayout *layout = new QGridLayout();
+
+	layout->addWidget(new QLabel("Size:"), 0, 0, 1, 3);
+
+	m_mbSizeX = new QSpinBox();
+	layout->addWidget(m_mbSizeX, 1, 0);
+
+	m_mbSizeY = new QSpinBox();
+	layout->addWidget(m_mbSizeY, 1, 1);
+
+	m_mbSizeZ = new QSpinBox();
+	layout->addWidget(m_mbSizeZ, 1, 2);
+
+	layout->addWidget(new QLabel("Location:"), 2, 0, 1, 3);
+
+	m_mbLocX = new QSpinBox();
+	layout->addWidget(m_mbLocX, 3, 0);
+
+	m_mbLocY = new QSpinBox();
+	layout->addWidget(m_mbLocY, 3, 1);
+
+	m_mbLocZ = new QSpinBox();
+	layout->addWidget(m_mbLocZ, 3, 2);
+
+	layout->addWidget(new QLabel("Rotation:"), 4, 0, 1, 3);
+
+	m_mbRotX = new QSpinBox();
+	layout->addWidget(m_mbRotX, 5, 0);
+
+	m_mbRotY = new QSpinBox();
+	layout->addWidget(m_mbRotY, 5, 1);
+
+	m_mbRotZ = new QSpinBox();
+	layout->addWidget(m_mbRotZ, 5, 2);
+
+	m_modifyBox->setLayout(layout);
+	m_modifyBox->setMaximumSize(250, 250);
 }
