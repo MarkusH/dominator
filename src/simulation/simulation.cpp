62add9e748b2a24cfbcab7f3853f13a5d856d2e8
rwaury <robert.waury@googlemail.com>
2011-06-29 00:28:18 +0200

    template load/save + example

--- src/simulation/simulation.cpp
+++ src/simulation/simulation.cpp
@@ -141,6 +141,68 @@ Simulation::~Simulation()
 	m_mouseAdapter.removeListener(this);
 }
 
+// TODO I didn't find the todo just paste this code where it belongs
+/* template load/save */
+/*
+void Simulation::saveTemplate(const std::string& fileName, __Object& object)
+{
+	using namespace rapidxml;
+	// create document
+	xml_document<> doc;
+
+	// create XML declaration
+	xml_node<>* declaration = doc.allocate_node(node_declaration);
+	doc.append_node(declaration);
+	declaration->append_attribute(doc.allocate_attribute("version", "1.0"));
+    declaration->append_attribute(doc.allocate_attribute("encoding", "utf-8"));
+
+
+	// create root element "template"
+	xml_node<>* t = doc.allocate_node(node_element, "template");
+	doc.append_node(t);
+	
+	__Object::save(object, t, &doc);
+
+	std::string s;
+	print(std::back_inserter(s), doc, 0);
+
+	// save document
+	std::ofstream myfile;
+	myfile.open (fileName.c_str());
+	myfile << s;
+	myfile.close();
+
+	// frees all memory allocated to the nodes
+	doc.clear();
+
+}
+
+void Simulation::loadTemplate(const std::string& fileName)
+{
+	using namespace rapidxml;
+
+	file<char> f(fileName.c_str());
+
+	char* m = f.data();
+	
+	// TODO add exception handling
+	xml_document<> doc;
+	doc.parse<0>(m);
+
+	// this is important so we don't parse the template tag but the object or compound tag
+	xml_node<>* nodes = doc.first_node();
+
+	// only the first tag is loaded the rest will be ignored
+	xml_node<>* node = nodes->first_node();
+	std::string type(node->name());
+
+	if (type == "object" || type == "compound") {
+			Object object = __Object::load(node);
+			add(object);
+		}
+}*/ /* template load/save END */
+
+
 void Simulation::save(const std::string& fileName)
 {
 	using namespace rapidxml;
@@ -193,6 +255,7 @@ void Simulation::load(const std::string& fileName)
 
 	char* m = f.data();
 	
+	// TODO add exception handling
 	xml_document<> doc;
 	doc.parse<0>(m);
 
