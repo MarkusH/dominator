37543f6398fea9a151e29ec388ee3dd06e486982
rwaury <robert.waury@googlemail.com>
2011-06-30 14:44:50 +0200

    fixed stupid typo

--- include/util/threadcounter.hpp
+++ include/util/threadcounter.hpp
@@ -7,7 +7,7 @@ int getThreadCount() {
 
 	unsigned threadcount = boost::thread::hardware_concurrency();
 
-	if(threadcount) return 2;
+	if(!threadcount) return 1;
 	if(threadcount > 8) return 8;
 	else return (int)threadcount;
 }
