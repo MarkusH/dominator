02cbea6e99e7ac9561267d5b06ddbec54355d09a
Markus Döllinger <markus_doellinger@t-online.de>
2011-07-11 20:42:46 +0200

    Modified tests.

--- src/unittests/m3dtest.cpp
+++ src/unittests/m3dtest.cpp
@@ -27,22 +27,32 @@ static inline float frand(float a = 0.0f, float b = 1.0f)
 void m3dTest::invertTest()
 {
 	using namespace m3d;
-	Mat4f matrix(frand(), frand(), frand(), frand(),
-				 frand(), frand(), frand(), frand(),
-				 frand(), frand(), frand(), frand(),
-				 frand(), frand(), frand(), frand());
+	Vec3f up(0.0f, 1.0f, 0.0f);
+	Vec3f dir(frand(), frand(), frand());
+	Vec3f pos(frand(), frand(), frand());
+	Mat4f matrix(up, dir, pos);
 	Mat4f inverse = matrix.inverse();
 	Mat4f result = matrix * inverse;
 	Mat4f identity = Mat4f::identity();
 
-	for (int x = 0; x < 4; ++x)
-		for (int y = 0; y < 4; ++y)
+	for (int x = 0; x < 4; ++x) {
+		for (int y = 0; y < 4; ++y) {
 			CPPUNIT_ASSERT(fabs(result[x][y] - identity[x][y]) < EPSILON);
+			CPPUNIT_ASSERT(result[x][y] == result[x][y]);
+		}
+	}
 }
 
 void m3dTest::grammSchmidtTest()
 {
-	CPPUNIT_ASSERT(true);
+	using namespace m3d;
+	Vec3f dir(frand(), frand(), frand());
+	Vec3f pos(frand(), frand(), frand());
+	Mat4f matrix = Mat4f::grammSchmidt(dir, pos);
+
+	CPPUNIT_ASSERT(fabs(matrix.getX() * matrix.getY()) < EPSILON);
+	CPPUNIT_ASSERT(fabs(matrix.getY() * matrix.getZ()) < EPSILON);
+	CPPUNIT_ASSERT(fabs(matrix.getX() * matrix.getZ()) < EPSILON);
 }
 
 
