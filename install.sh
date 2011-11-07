b4d63201051037f1a7d2de61cdecab37417541e9
Markus Holtermann <markus@markusholtermann.de>
2011-11-07 21:58:14 +0100

    finalize installer script

--- install.sh
+++ install.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-/usr/bin/env
-
 if [ ! $UID -eq 0 ]; then
-    zenity --error --text="You need to be root to run the install routine!"
+    echo "You need to be root to run the install routine!"
     exit 1
 fi
 
 /usr/bin/env > /dev/null
 SUDO_HOME="$(cut -d':' -f1,6 /etc/passwd | egrep ^${SUDO_USER}: | cut -d':' -f2)/"
-DST="${SUDO_HOME}/dominator/"
+DST="${SUDO_HOME}dominator/"
 FILEPATH="$(readlink -f "${SUDO_COMMAND}")"
 SRC="$(dirname "${FILEPATH}")/.install/"
-echo $FILEPATH
-echo $SRC
-
-exit 0
 
 apt-get install libglew1.5 libqt4 lib3ds libboost-thread1.40.0 libboost-filesystem1.40.0
 mkdir -p "${DST}"
@@ -26,4 +20,6 @@ cp -r "${SRC}data" "${DST}"
 cp -r "${SRC}licenses" "${DST}"
 cp "${SRC}LICENSE" "${SRC}README" "${DST}"
 
-echo -e "\n\n# Dominator binary\nexport PATH=${PATH}:${DST}\n" >> "${SUDO_HOME}.bashrc"
+chown -R ${SUDO_UID}:${SUDO_GID} "${DST}"
+
+echo -e "\n# Dominator binary\nexport PATH=${PATH}:${DST}\n" >> "${SUDO_HOME}.bashrc"
