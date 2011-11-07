5c0fb1b0a51ea621ebdaeb3fba6fee79f6a3c9db
Markus Holtermann <markus@markusholtermann.de>
2011-11-07 17:55:19 +0100

    adding linux installer scripts

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+/usr/bin/env
+
+if [ ! $UID -eq 0 ]; then
+    zenity --error --text="You need to be root to run the install routine!"
+    exit 1
+fi
+
+/usr/bin/env > /dev/null
+SUDO_HOME="$(cut -d':' -f1,6 /etc/passwd | egrep ^${SUDO_USER}: | cut -d':' -f2)/"
+DST="${SUDO_HOME}/dominator/"
+FILEPATH="$(readlink -f "${SUDO_COMMAND}")"
+SRC="$(dirname "${FILEPATH}")/.install/"
+echo $FILEPATH
+echo $SRC
+
+exit 0
+
+apt-get install libglew1.5 libqt4 lib3ds libboost-thread1.40.0 libboost-filesystem1.40.0
+mkdir -p "${DST}"
+
+cp "${SRC}libs/*" "/usr/lib/"
+cp "${SRC}dominator" "${DST}"
+cp -r "${SRC}data" "${DST}"
+cp -r "${SRC}licenses" "${DST}"
+cp "${SRC}LICENSE" "${SRC}README" "${DST}"
+
+echo -e "\n\n# Dominator binary\nexport PATH=${PATH}:${DST}\n" >> "${SUDO_HOME}.bashrc"
